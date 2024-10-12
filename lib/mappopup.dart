71ea9d29485997dc7ccbe41a18df2d12d55baf86
EthanZ <ethan.q.zheng0529@gmail.com>
2024-10-12 10:18:03 -0700

    mappopup

--- lib/mappopup.dart
+++ lib/mappopup.dart
@@ -1,27 +1,107 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:location/location.dart';
+import 'dart:ui';
 
-class CustomButton extends StatelessWidget {
-  final String platform;
-  final VoidCallback onPressed;
-  const CustomButton({super.key, required this.platform, required this.onPressed});
+class Popup extends StatelessWidget {
+  
+  final VoidCallback runOnPressed;
+  final String eventTitle;
+  final String eventDescription;
+  final String eventDate;
+  final String eventStartTime;
+  final String eventEndTime; 
+
+  const Popup({super.key, required this.runOnPressed, required this.eventTitle, required this.eventDescription, required this.eventDate, required this.eventStartTime, required this.eventEndTime});
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-      Text("Press the below button to follow me on $platform"),
-      ElevatedButton(
-        onPressed: () {
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(
-              content: Text("Pressed Follow on $platform button"),
-              duration: const Duration(seconds: 1),
+    
+    return ClipRRect(
+      borderRadius: BorderRadius.only(
+        topLeft: Radius.circular(16.0),
+        bottomLeft: Radius.circular(16.0),
+        bottomRight: Radius.circular(16.0),
+        topRight: Radius.circular(16.0),
+      ),
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+        child: Container(
+          width: MediaQuery.of(context).size.width * 0.3, // 30% of screen width
+          padding: EdgeInsets.all(16.0),
+          decoration: BoxDecoration(
+            color: Colors.white.withOpacity(0.1), // Semi-transparent background
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(16.0),
+              bottomLeft: Radius.circular(16.0),
             ),
-          );
-          onPressed();
-        },
-        child: Text("Follow on $platform"),
-      )
-    ]));
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black26,
+                blurRadius: 10.0,
+                spreadRadius: 5.0,
+              ),
+            ],
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image(image: NetworkImage('https://banner2.cleanpng.com/20180810/eji/adab626c847044a992aa49c9aaf003bb.webp'),)
+                ,Text(
+                  eventTitle,
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+              ]),
+              
+              SizedBox(height: 8.0),
+              Text(
+                "Details",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)
+              
+              ),
+              SizedBox(height: 0.0),
+              Container(
+                //width: MediaQuery.of(context).size.width * 0.3, // 30% of screen width
+                height: 150.0,
+                padding: EdgeInsets.all(8.0),
+                margin: EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(16.0),
+                    bottomLeft: Radius.circular(16.0),
+                    topRight: Radius.circular(16.0),
+                    bottomRight: Radius.circular(16.0),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black26,
+                      blurRadius: 10.0,
+                      spreadRadius: 5.0,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 16.0),
+              Text("Start Time: $eventStartTime"),
+              Text("End Time: $eventEndTime"),
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () {
+                  runOnPressed();
+                },
+                child: Text('Register'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+        
+        
+      
   }
 }
